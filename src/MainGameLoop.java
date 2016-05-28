ecc418c019d22236baa82eaf44129e420467922f
Husni Munaya <husnimun@gmail.com>
2016-05-28 21:06:12 +0700

    Rendering with index buffers

--- src/MainGameLoop.java
+++ src/MainGameLoop.java
@@ -12,18 +12,10 @@ public class MainGameLoop {
         Loader loader = new Loader();
         Renderer renderer = new Renderer();
 
-        float[] vertices = {
-                -0.5f, 0.5f, 0f,
-                -0.5f, -0.5f, 0f,
-                0.5f, -0.5f, 0f,
+        float[] vertices = { -0.5f, 0.5f, 0, -0.5f, -0.5f, 0, 0.5f, -0.5f, 0, 0.5f, 0.5f, 0f };
+        int[] indices = { 0,1,3, 3,1,2 };
 
-                0.5f, -0.5f, 0f,
-                0.5f, 0.5f, 0f,
-                -0.5f, 0.5f, 0f,
-
-        };
-
-        RawModel model = loader.loadToVAO(vertices);
+        RawModel model = loader.loadToVAO(vertices, indices);
         while(!Display.isCloseRequested()) {
             renderer.prepare();
             renderer.render(model);
